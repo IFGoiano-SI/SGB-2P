b37c25afe566586c473bde8166b1301ada9e6772
Matheus Vieira <theusvsilv@gmail.com>
2024-11-06 23:57:03 -0300

    TESTE

--- SGB-2P/sgb-2p/src/classes/Emprestimo.java
+++ SGB-2P/sgb-2p/src/classes/Emprestimo.java
@@ -6,7 +6,7 @@ public class Emprestimo {
     int data_emp;
     int data_dev;
 
-    Emprestimo(Livro livro, Cliente cliente, int data_emp, int data_dev) {
+    public Emprestimo(Livro livro, Cliente cliente, int data_emp, int data_dev) {
         this.livro = livro;
         this.cliente = cliente;
         this.data_emp = data_emp;
